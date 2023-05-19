63bece2e305b60310099f3bc9fefaac006381e47
Serega Koval <seregakoval@MacBook-Pro-Serega.local>
2023-05-19 13:25:10 +0300

    init-files

--- src/pages/auth/signup/success.tsx
+++ src/pages/auth/signup/success.tsx
@@ -1,7 +1,5 @@
-import { SignUpSuccessView } from '@/components/views/auth/signup-success-view/signup-success-view';
-
 export interface SignUpSuccessRouteProps {}
 
 export default function SignUpSuccessRoute(props: SignUpSuccessRouteProps) {
-  return <SignUpSuccessView />;
+  return <>231231</>;
 }
